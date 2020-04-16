6b5e82a54cdfe6a2344c4fe39b3ff91981f21c3b
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-04-16 20:48:08 -0300

    Adds the rest of code for CC #69

--- src/cc_69-evolut_steering_behav/sketch.js
+++ src/cc_69-evolut_steering_behav/sketch.js
@@ -1,23 +1,78 @@
-let v;
+let vehicles = [];
+const foodPieces = [];
+const poisonPieces = [];
+let debugCheckbox;
+
+function newRandomPosition() {
+    const x = random(width);
+    const y = random(height);
+    return createVector(x, y);
+}
+
+function newFood() {
+    foodPieces.push(newRandomPosition());
+}
+
+function newPoison() {
+    poisonPieces.push(newRandomPosition());
+}
+
+function drawPiece(x, y) {
+    noStroke();
+    ellipse(x, y, 4, 4);
+}
 
 function setup() {
     createCanvas(640, 360);
-    v = new Vehicle(width / 2, height / 2);
+
+    for (let i=0; i<50; i++) {
+        const { x, y } = newRandomPosition();
+        vehicles.push(new Vehicle(x, y));
+    }
+
+    for (let i=0; i<40; i++) { newFood() }
+
+    for (let i=0; i<20; i++) { newPoison() }
+
+    debugCheckbox = createCheckbox('Debug');
+    debugCheckbox.checked(true);
 }
 
 function draw() {
     background(51);
 
-    let mouse = createVector(mouseX, mouseY);
+    if (random(1) < 0.1) { newFood() }
 
-    // Draw an ellipse at the mouse position
-    fill(127);
-    stroke(200);
-    strokeWeight(2);
-    ellipse(mouse.x, mouse.y, 48, 48);
+    if (random(1) < 0.01) { newPoison() }
+
+    foodPieces.forEach(({ x, y }) => {
+        fill(0,255,0);
+        drawPiece(x, y);
+    });
+
+    poisonPieces.forEach(({ x, y }) => {
+        fill(255,0,0);
+        drawPiece(x, y);
+    });
 
     // Call the appropriate steering behaviors for our agents
-    v.seek(mouse);
-    v.update();
-    v.display();
+    for (let index=vehicles.length-1; index >= 0; index--) {
+        const vehicle = vehicles[index];
+        vehicle.boundaries();
+        vehicle.behaviors(foodPieces, poisonPieces);
+        vehicle.update();
+        vehicle.display(debugCheckbox.checked());
+
+        const newVehicle = vehicle.clone();
+        if (newVehicle) {
+            vehicles.push(newVehicle);
+        }
+
+        if (vehicle.dead()) {
+            const x = vehicle.position.x;
+            const y = vehicle.position.y;
+            foodPieces.push(createVector(x, y));
+            vehicles.splice(index, 1);
+        }
+    }
 }
--- src/cc_69-evolut_steering_behav/vehicle.js
+++ src/cc_69-evolut_steering_behav/vehicle.js
@@ -1,21 +1,47 @@
+const mutationRate = 0.01;
+
 class Vehicle {
-    constructor(x, y) {
+    constructor(x, y, dna) {
         this.acceleration = createVector(0, 0);
         this.velocity = createVector(0, -2);
         this.position = createVector(x, y);
-        this.r = 6;
-        this.maxspeed = 8;
-        this.maxforce = 0.2;
+        this.r = 4;
+        this.maxspeed = 5;
+        this.maxforce = 0.5;
+
+        this.health = 1;
+
+        if (dna) {
+            this.dna = dna.map((gene, index) => {
+                let mutation = 0;
+                if (random(1) < mutationRate) {
+                    if (index > 1) {
+                        mutation = random(-10, 10);
+                    } else {
+                        mutation = random(-0.1, 0.1)
+                    }
+                }
+                return gene + mutation;
+            });
+        } else {
+            this.dna = [
+                random(-2,2), // food weight
+                random(-2,2), // poison weight
+                random(0, 100), // food perception
+                random(0, 100), // poison perception
+            ];
+        }
     }
 
     // Method to update location
     update() {
+        this.health -= 0.005;
         // Update velocity
         this.velocity.add(this.acceleration);
         // Limit speed
         this.velocity.limit(this.maxspeed);
         this.position.add(this.velocity);
-        // Reset accelerationelertion to 0 each cycle
+        // Reset acceleration to 0 each cycle
         this.acceleration.mult(0);
     }
 
@@ -24,36 +50,125 @@ class Vehicle {
         this.acceleration.add(force);
     }
 
+    behaviors(good, bad) {
+        const steerG = this.eat(good, 0.2, this.dna[2]);
+        const steerB = this.eat(bad, -1, this.dna[3]);
+
+        steerG.mult(this.dna[0]);
+        steerB.mult(this.dna[1]);
+
+        this.applyForce(steerG);
+        this.applyForce(steerB);
+    }
+
+    clone() {
+        if (random(1) < 0.002) {
+            const { x, y } = this.position;
+            return new Vehicle(x, y, this.dna);
+        }
+        return null;
+    }
+
+    eat(list, nutrition, perception) {
+        let record = Infinity;
+        let closest = null;
+
+        for (let index=list.length-1; index >= 0; index--) {
+            const item = list[index];
+            const distance = this.position.dist(item);
+            
+            if (distance < this.maxspeed) {
+                list.splice(index, 1);
+                this.health += nutrition;
+            } else if (distance < record && distance < perception) {
+                record = distance;
+                closest = item;
+            }
+        }
+
+        return closest ? this.seek(closest) : createVector(0,0);
+    }
+
     // A method that calculates a steering force towards a target
     // STEER = DESIRED MINUS VELOCITY
     seek(target) {
-
-        var desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
+        const desired = p5.Vector.sub(target, this.position); // A vector pointing from the location to the target
 
         // Scale to maximum speed
         desired.setMag(this.maxspeed);
 
         // Steering = Desired minus velocity
-        var steer = p5.Vector.sub(desired, this.velocity);
+        const steer = p5.Vector.sub(desired, this.velocity);
         steer.limit(this.maxforce); // Limit to maximum steering force
 
-        this.applyForce(steer);
+        return steer;
     }
 
-    display() {
+    dead() {
+        return this.health <= 0;
+    }
+
+    display(debug = false) {
         // Draw a triangle rotated in the direction of velocity
-        let theta = this.velocity.heading() + PI / 2;
-        fill(127);
-        stroke(200);
-        strokeWeight(1);
+        const theta = this.velocity.heading() + PI / 2;
         push();
         translate(this.position.x, this.position.y);
         rotate(theta);
+        
+        if (debug) {
+            this.displayDebug();
+        }
+
+        const gr = color(0, 255, 0);
+        const rd = color(255, 0, 0);
+        const col = lerpColor(rd, gr, this.health);
+
+        fill(col);
+        stroke(col);
+        strokeWeight(1);
         beginShape();
         vertex(0, -this.r * 2);
         vertex(-this.r, this.r * 2);
         vertex(this.r, this.r * 2);
         endShape(CLOSE);
+
         pop();
+    }
+
+    displayDebug() {
+        strokeWeight(3);
+        stroke(0,255,0);
+        noFill();
+        line(0, 0, 0, -this.dna[0]*25);
+        ellipse(0, 0, this.dna[2] * 2);
+        strokeWeight(2);
+        stroke(255,0,0);
+        line(0, 0, 0, -this.dna[1]*25);
+        ellipse(0, 0, this.dna[3] * 2);
+    }
+
+    boundaries() {
+        const d = 25;
+        let desired = null;
+
+        if (this.position.x < d) {
+            desired = createVector(this.maxspeed, this.velocity.y);
+        } else if (this.position.x > width - d) {
+            desired = createVector(-this.maxspeed, this.velocity.y);
+        }
+
+        if (this.position.y < d) {
+            desired = createVector(this.velocity.x, this.maxspeed);
+        } else if (this.position.y > height - d) {
+            desired = createVector(this.velocity.x, -this.maxspeed);
+        }
+
+        if (desired !== null) {
+            desired.normalize();
+            desired.mult(this.maxspeed);
+            let steer = p5.Vector.sub(desired, this.velocity);
+            steer.limit(this.maxforce);
+            this.applyForce(steer);
+        }
     }
 }
